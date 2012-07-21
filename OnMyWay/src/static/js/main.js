7dca833942cf46b6d6a6699e9ab1f2ff4e1b9783
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-07-21 16:33:16 -0400

    HTML5 Geolocation

--- OnMyWay/src/static/js/main.js
+++ OnMyWay/src/static/js/main.js
@@ -2,10 +2,31 @@
  * OnMyWay Frontend JS
  */
 
+'use strict';
 $(function() {
 
-  omw.init = function() {
-		console.log("Hello world");
+  var omw = omw || {
+    position: null
 	};
+
+  omw.init = function() {
+
+		omw.getLoc(function(position) {		// If we can get the location
+      var lat = position.coords.latitude,
+        lng = position.coords.longitude;
+  		$('#from').addClass('cur-log').val('My Location');
+    });
+
+	};
+
+  omw.getLoc = function(cb, error) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+      omw.position = position;
+      cb(position);
+    }, (error || function(msg) { console.log(msg); }));
+	};
+
+  // Kick things off
+  omw.init();
 
 });
