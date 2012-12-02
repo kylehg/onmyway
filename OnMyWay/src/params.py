98fb80e08cf657cbc49603948f0558f40fae11ac
Dina Lamdany <dinalamdany@gmail.com>
2012-12-01 19:57:38 -0500

    real api keys

--- OnMyWay/src/params.py
+++ OnMyWay/src/params.py
@@ -3,7 +3,7 @@
 class MapParams(object):
     # Application's Google Maps API key. This key identifies your application 
     # for purposes of quota management.
-    API_KEY = 'AIzaSyAIyNInDkYpSNjc5DJh9ViNqyXD1AusiI8'
+    API_KEY = 'AIzaSyBSg_HLNGG2RoYolQFnLgge8AxuThfTo-E'
 
     # URL Request Addresses
     API_BASE_URL = 'http://maps.googleapis.com/maps/api'
@@ -15,21 +15,9 @@ class MapParams(object):
 
 class YelpParams(object):
     #Authentication for Yelp
-    YELP_CONSUMER_KEY = '83MKEDpPxnNnaYXUPBKmxw'
-    YELP_CONSUMER_SECRET = '6Shbk8HTNmX6a8mmDFa7gNWm4ko'
-    YELP_TOKEN = 'oUjGEtvAmpA7XJWFf627_-bxYcCQqETd'
-    YELP_TOKEN_SECRET = 'GNX66S0xKzGP4BYGkYIXrT2sG8E'
+    YELP_CONSUMER_KEY = 'eHr2SL2LJYafgwfwP22NoQ'
+    YELP_CONSUMER_SECRET = 'yKepW4_EPYfeE7dU4viGI1u2LDw'
+    YELP_TOKEN = 'xY2Rf7AH-bM7l6JR2-pd7FjVGQbhzhxE'
+    YELP_TOKEN_SECRET = 'G5UhHftfvWMJ0WzJTyxPhGETnCc'
 
-class YelpParamsWaseem(object):
-    # OAuth2 Authentication Params for Yelp
-    YELP_CONSUMER_KEY = 'yDbF4bpNdDWW2clY7VnXHw'
-    YELP_CONSUMER_SECRET = 'L4jLP4q3kUrcwCJzfHKffAWGi0I'
-    YELP_TOKEN = 'SS4UfObtS8d8E6BYsDHNlsc0ZjGsNjWt'
-    YELP_TOKEN_SECRET = '-U1arPRU3EpSCPAuETyU3MxC_zA'
 
-class YelpParamsDina(object):
-    # OAuth2 Authentication Params for Yelp
-    YELP_CONSUMER_KEY = 'xP3Xi0nxQT3_haz3lATTdw'
-    YELP_CONSUMER_SECRET = 'Jt2TA5x9FrYC1cvJtpOjNK5IrNE'
-    YELP_TOKEN = '6fIyzoZejx7Jdwob5aJ3QGdh2X8y3Ol5'
-    YELP_TOKEN_SECRET = 'No0jktoh-lXztoID9mTof3BllOA'
